f2d78a450e424502419242fb578441a8708b78ed
Nimatullah Razmjo <razmjo@netlinks.ws>
2018-09-08 15:29:24 +0430

    set constan value to test

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,7 @@ test("First test",() => {
 
 describe('Test PATH /',() =>{
   it('Should return 200 if the server is run',() => {
-     
+    const status = 200;
+    expect(status).toBe(200)     
   });
 });
